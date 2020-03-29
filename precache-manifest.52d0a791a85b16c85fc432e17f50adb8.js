2812fe3cab099cb96230b021deb155474fd683d6
Kir Novak <hermes.coder@gmail.com>
2020-03-30 03:42:52 +1100

    Updates

---
+++ precache-manifest.52d0a791a85b16c85fc432e17f50adb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dbb68c685c1ce18365aaee1efe14c0",
+    "url": "/kirnovak/pages/index.html"
+  },
+  {
+    "revision": "ad0b6d3327a106d02522",
+    "url": "/kirnovak/pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecda60e66fc426d0f97a",
+    "url": "/kirnovak/pages/static/js/2.5a702c1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kirnovak/pages/static/js/2.5a702c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0b6d3327a106d02522",
+    "url": "/kirnovak/pages/static/js/main.fa1e45ac.chunk.js"
+  },
+  {
+    "revision": "bf0fd4715a3866c08ac2",
+    "url": "/kirnovak/pages/static/js/runtime-main.ce38cc49.js"
+  }
+]);
